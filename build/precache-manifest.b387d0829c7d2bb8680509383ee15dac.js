534aea2ef401a045ec500975e3e1abbcdd7e1364
bokaderohit98 <bokaderohit98@gmail.com>
2019-10-15 12:59:56 +0530

    chore(build): added ui build

---
+++ build/precache-manifest.b387d0829c7d2bb8680509383ee15dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6980e63806d63f3ec2c7e0459c134fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20a5df3e91a38e62e340",
+    "url": "/static/js/2.26c58d50.chunk.js"
+  },
+  {
+    "revision": "7075b831c807cd2d2941",
+    "url": "/static/js/main.301850e9.chunk.js"
+  },
+  {
+    "revision": "1c29cc4e85277318a0bd",
+    "url": "/static/js/runtime-main.cdc8144b.js"
+  },
+  {
+    "revision": "61a7eb8b6fb8e7d44cff3e75bb4b4332",
+    "url": "/static/media/error.61a7eb8b.png"
+  }
+]);
