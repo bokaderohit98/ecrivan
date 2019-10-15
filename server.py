c4f95d6ec48ef78e6ad0bb4baf217296716e666b
bokaderohit98 <bokaderohit98@gmail.com>
2019-10-15 12:59:37 +0530

    feat(server): added route to serve static files

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from flask import Flask, request, send_file, send_from_directory, abort, jsonify
 from flask_cors import CORS
+import os
 from utils.storyGenerator import storyGenerator
 
 app = Flask(__name__, static_folder='build')
 CORS(app)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != '' and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+
 
 @app.route('/api', methods=['GET'])
 def ecrivan():
